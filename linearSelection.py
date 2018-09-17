f111c7b1fd7b74a2e2f879773f7e0c9d99ca3421
Bianca <33526317+biafrag@users.noreply.github.com>
2018-09-16 21:25:19 -0300

    "Inclusão de relatorio e de alguns testes"

--- linearSelection.py
+++ linearSelection.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3.6
 
+from random import randrange, uniform
+import time
 
 def groupIn5(l):
     group = []
@@ -32,8 +34,10 @@ def getLR(l,mediana):
     return L,R
 
 
-def __linearSelectionRecusion(l,k):    
+def __linearSelectionRecusion(l,k):
+    # Função que vai separando em grupos de 5 até achar a mediana
     m = linearMedian(l)
+    # Função que separa L e R
     L,R = getLR(l,m)
     if k == len(L) + 1: # elemento eh a mediana
         return m
@@ -48,9 +52,41 @@ def linearSelection(l,k):
     assert( type(l) == list)
     return __linearSelectionRecusion(l,k)
 
-a = [7,4,1,2,3,5,6,89,20,22,14,12,25,11,32,33,44]
-k = 4
-e = linearSelection(a,k)
-a.sort()
-print("vetor:",a)
-print(e , k)
+def Testes ():
+    # Teste para 10 instâncias diferentes
+    for i in range (0,10):
+        vet = []
+        # Cada instância vai ter quantidade de elementos diferentes (1000,2000.....10000)
+        for j in range (0,(i+1)*1000):
+            vet.append(uniform(1, 100000))  # faixa de ponto flutuante
+
+        #Calculando k, sendo metade da lista arredondado para baixo
+        k = len(vet)//2
+
+        #Calculando tempo de execução do LinearSelection
+        inicio = time.time()
+        m = linearSelection(vet, k)
+        fim = time.time()
+        somaLinear = fim - inicio
+
+        # Calculando tempo de execução do SortSelection
+        inicio = time.time()
+        vet.sort()
+        m2  = vet[k-1]
+        fim = time.time()
+        somaSort = fim - inicio
+
+        print("Tempo médio total do algoritmo Linear Selection para ",(i+1)*1000, " elementos: ")
+        print("{:.2e}".format(somaLinear))
+        print("Tempo médio total do algoritmo Sort Selection para ",(i+1)*1000, " elementos: ")
+        print("{:.2e}".format(somaSort))
+
+
+#a = [7,4,1,2,3,5,6,89,20,22,14,12,25,11,32,33,44]
+#k = 4
+#e = linearSelection(a,k)
+#a.sort()
+#print("vetor:",a)
+#print(e , k)
+Testes()
+#Teste
